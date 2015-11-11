5dbb58df044eff8edeaff72ac05c0de772ec768f
Pedro Matos <pedrohugorm@gmail.com>
2015-11-11 21:32:50 -0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -14,6 +14,12 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+REM Package restore
+call %NuGet% restore Gallifrey.SharedKernel\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore Gallifrey.Infrastructure\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore Gallifrey.Persistence\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore Gallifrey.RestApiController\packages.config -OutputDirectory %cd%\packages -NonInteractive
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Gallifrey.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
