df0f23e531534edfed8a06f6a9ab6bc7312531dd
Pedro Matos <pedrohugorm@gmail.com>
2015-11-14 19:54:42 -0200

    removed usings

--- Gallifrey.RestApiController/Global.asax.cs
+++ Gallifrey.RestApiController/Global.asax.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web;
 
 namespace Gallifrey.RestApi
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
